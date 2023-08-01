8e83d65c30d349ace196c5cdf9fc99ce585e1488
Ken "TheNugget" Yılmaz <95768094+TheCodeNugget@users.noreply.github.com>
2023-08-01 22:36:23 +0300

    feat: Variant-2 Colourset-1 Complete (#4)
    * feat: V2/C9 Complete
    
    * feat: V2/C9/M Complete
    
    * feat: V2/S9 Complete
    
    * feat: V2/15x15L Complete
    
    * feat: V2/18x18LI Complete

---
+++ rnc_apartments/Variant2/Cube9/config.cpp
@@ -0,0 +1,184 @@
+#define _ARMA_
+#include "hitpoint_macros.hpp"
+
+//(13 Enums)
+enum {
+	destructengine = 2,
+	destructdefault = 6,
+	destructwreck = 7,
+	destructtree = 3,
+	destructtent = 4,
+	stabilizedinaxisx = 1,
+	stabilizedinaxesxyz = 4,
+	stabilizedinaxisy = 2,
+	stabilizedinaxesboth = 3,
+	destructno = 0,
+	stabilizedinaxesnone = 0,
+	destructman = 5,
+	destructbuilding = 1
+};
+
+class CfgPatches {
+	class rnc_apartments_variant2_cube9 {
+		addonRootClass = "rnc_apartments";
+		requiredAddons[] = {"rnc_main"};
+		requiredVersion = 0.1;
+		units[] = {
+			"Land_rnc_apt_v2_c9_c1_f3",
+			"Land_rnc_apt_v2_c9_c1_f4",
+			"Land_rnc_apt_v2_c9_c1_f5",
+			"Land_rnc_apt_v2_c9_c1_f6",
+			"Land_rnc_apt_v2_c9_c1_f3_m",
+			"Land_rnc_apt_v2_c9_c1_f3_m",
+			"Land_rnc_apt_v2_c9_c1_f3_m",
+			"Land_rnc_apt_v2_c9_c1_f3_m"
+		};
+		weapons[] = {};
+	};
+};
+class CfgVehicles {
+	class House_Small_F;
+
+	class Land_rnc_apt_v2_c9_c1_f3: House_Small_F {
+		scope = 2;
+		displayName = "Apartment V2/C9/C1/F3";
+		model = "rnc_apartments\variant2\cube9\rnc_apt_v2_c9_c1_f3.p3d";
+
+		editorCategory = "rnc_apartments";
+		editorSubCategory = "rnc_v2_c9";
+		
+		icon = "iconObject_1x2";
+		mapsize = 4;
+
+		destrType = "DestructDefault";
+		armor = 1000;
+
+		numberOfWindows = 8;
+		numberOfDoors = 1;
+
+		class Hitpoints {
+			NORMAL_GLASS_HITPOINT(1,0.001,0.4)
+			NORMAL_GLASS_HITPOINT(2,0.001,0.4)
+			NORMAL_GLASS_HITPOINT(3,0.001,0.4)
+			NORMAL_GLASS_HITPOINT(4,0.001,0.4)
+			NORMAL_GLASS_HITPOINT(5,0.001,0.4)
+			NORMAL_GLASS_HITPOINT(6,0.001,0.4)
+			NORMAL_GLASS_HITPOINT(7,0.001,0.4)
+			NORMAL_GLASS_HITPOINT(8,0.001,0.4)
+		};
+
+		class Damage {
+			tex[] = {
+				// Window textures
+				"A3\Structures_F\Data\Windows\window_set_CA.paa",
+				"A3\Structures_F\Data\Windows\destruct_half_window_set_CA.paa",
+
+				// Grey color
+				"#(argb,8,8,3)color(0.501961,0.501961,0.501961,1.0,co)",
+				"#(argb,8,8,3)color(0.294118,0.294118,0.294118,1.0,co)",
+
+				// Brown color
+				"#(argb,8,8,3)color(0.501961,0.25098,0,1.0,co)",
+				"#(argb,8,8,3)color(0.392157,0.196078,0,1.0,co)",
+
+				// Yellow color
+				"#(argb,8,8,3)color(1,1,0.501961,1.0,co)",
+				"#(argb,8,8,3)color(0.513725,0.513725,0.203922,1.0,co)",
+
+				// Light grey color
+				"#(argb,8,8,3)color(0.752941,0.752941,0.752941,1.0,co)",
+				"#(argb,8,8,3)color(0.478431,0.478431,0.478431,1.0,co)",
+
+				// Red color
+				"#(argb,8,8,3)color(1,0,0,1.0,co)",
+				"#(argb,8,8,3)color(0.701961,0,0,1.0,co)"
+			};
+
+			mat[] = {
+				"A3\Structures_F\Data\Windows\window_set.rvmat",
+				"A3\Structures_F\Data\Windows\destruct_half_window_set.rvmat",
+				"A3\Structures_F\Data\Windows\destruct_full_window_set.rvmat"
+			};
+		};
+
+		class AnimationSources {
+			class door_1_source {
+				source = user;
+				initPhase = 0;
+				animPeriod = 1;
+				sound = "GenericDoorsSound";
+			};
+
+			class glass_1_source {
+				source = "Hit";
+				hitpoint = "glass_1_hitpoint";
+				raw = 1;
+			};
+			class glass_2_source : glass_1_source { hitpoint = "glass_2_hitpoint"; };
+			class glass_3_source : glass_1_source { hitpoint = "glass_3_hitpoint"; };
+			class glass_4_source : glass_1_source { hitpoint = "glass_4_hitpoint"; };
+			class glass_5_source : glass_1_source { hitpoint = "glass_5_hitpoint"; };
+			class glass_6_source : glass_1_source { hitpoint = "glass_6_hitpoint"; };
+			class glass_7_source : glass_1_source { hitpoint = "glass_7_hitpoint"; };
+			class glass_8_source : glass_1_source { hitpoint = "glass_8_hitpoint"; };
+		};
+		
+		class UserActions {
+			class openDoor_1 {
+				displayNameDefault = "<img image='\A3\Ui_f\data\IGUI\Cfg\Actions\open_door_ca.paa' size='2.5' />"; // This is displayed in the center of the screen just below crosshair. In this case it's an icon, not a text.
+				displayName = "$STR_DN_OUT_O_DOOR"; // Label of the action used in the action menu itself.
+				position = Door_1_Trigger; // Point in Memory lod in p3d around which the action is available.
+				priority = 0.4; // Priority coefficient used for sorting action in the action menu.
+				radius = 1.75; // Range around the above defined point in which you need to be to access the action.
+				aiMaxRange = 5.25;
+				onlyForPlayer = false; // Defines if the action is available only to players or AI as well.
+				condition = (this animationPhase 'Door_1_rot') < 0.5; // Condition for showing the action in action menu. In this case it checks if the door is closed and if the part of the house in which the door is located hasn't been destroyed yet).
+				statement = ([this, 'Door_1_rot'] call BIS_fnc_DoorNoHandleOpen); // Action taken when this action is selected in the action menu. In this case it calls a function that opens the door.
+			};
+			class closeDoor_1: openDoor_1 {
+				displayName = "$STR_DN_OUT_C_DOOR";
+				priority = 0.2;
+				condition = (this animationPhase 'Door_1_rot') >= 0.5; // Checks if the door is currently open and not destroyed.
+				statement = ([this, 'Door_1_rot'] call BIS_fnc_DoorNoHandleClose);
+			};
+		};
+		// Here are references binding specific positions in Path lod in p3d to specific actions from "class UserActions" for AI to know when to use which doors. The actionBegin# and ActionEnd# is a hardcoded naming system.
+		ActionBegin1 = openDoor_1;
+		ActionEnd1 = openDoor_1;
+	};
+	
+	class Land_rnc_apt_v2_c9_c1_f4: Land_rnc_apt_v2_c9_c1_f3{
+		displayName = "Apartment V2/C9/C1/F4";
+		model = "rnc_apartments\variant2\cube9\rnc_apt_v2_c9_c1_f4.p3d";
+	};
+
+	class Land_rnc_apt_v2_c9_c1_f5: Land_rnc_apt_v2_c9_c1_f3{
+		displayName = "Apartment V2/C9/C1/F5";
+		model = "rnc_apartments\variant2\cube9\rnc_apt_v2_c9_c1_f5.p3d";
+	};
+
+	class Land_rnc_apt_v2_c9_c1_f6: Land_rnc_apt_v2_c9_c1_f3{
+		displayName = "Apartment V2/C9/C1/F6";
+		model = "rnc_apartments\variant2\cube9\rnc_apt_v2_c9_c1_f6.p3d";
+	};
+	
+	class Land_rnc_apt_v2_c9_c1_f3_m: Land_rnc_apt_v2_c9_c1_f3{
+		displayName = "Apartment V2/C9/C1/F3/M";
+		model = "rnc_apartments\variant2\cube9\rnc_apt_v2_c9_c1_f3_m.p3d";
+	};
+	
+	class Land_rnc_apt_v2_c9_c1_f4_m: Land_rnc_apt_v2_c9_c1_f3{
+		displayName = "Apartment V2/C9/C1/F4/M";
+		model = "rnc_apartments\variant2\cube9\rnc_apt_v2_c9_c1_f4_m.p3d";
+	};
+	
+	class Land_rnc_apt_v2_c9_c1_f5_m: Land_rnc_apt_v2_c9_c1_f3{
+		displayName = "Apartment V2/C9/C1/F5/M";
+		model = "rnc_apartments\variant2\cube9\rnc_apt_v2_c9_c1_f5_m.p3d";
+	};
+	
+	class Land_rnc_apt_v2_c9_c1_f6_m: Land_rnc_apt_v2_c9_c1_f3{
+		displayName = "Apartment V2/C9/C1/F6/M";
+		model = "rnc_apartments\variant2\cube9\rnc_apt_v2_c9_c1_f6_m.p3d";
+	};
+};
