76243721d4583122bee2bc7726c02ecf5da4088f
TheCodeNugget <kyilmaz190300@gmail.com>
2023-12-23 17:12:46 +0300

    feat: Diesel Locomotive Made

---
+++ rnc_misc/railways/config.cpp
@@ -0,0 +1,79 @@
+#define _ARMA_
+
+//(13 Enums)
+enum {
+	destructengine = 2,
+	destructdefault = 6,
+	destructwreck = 7,
+	destructtree = 3,
+	destructtent = 4,
+	stabilizedinaxisx = 1,
+	stabilizedinaxesxyz = 4,
+	stabilizedinaxisy = 2,
+	stabilizedinaxesboth = 3,
+	destructno = 0,
+	stabilizedinaxesnone = 0,
+	destructman = 5,
+	destructbuilding = 1
+};
+
+class CfgPatches {
+	class rnc_railways {
+		addonRootClass = "rnc_misc";
+		requiredAddons[] = {"rnc_main"};
+		requiredVersion = 0.1;
+		units[] = {
+			
+			};
+		weapons[] = {};
+	};
+};
+class CfgVehicles {
+	
+	class ThingX;
+	class Land_rnc_diesel_locomotive_01: ThingX {
+		author = "RUMM & COKE Team / Quaternius";
+		mapSize = 6.1;
+		class SimpleObject {
+			eden = 0;
+			animate[] = {};
+			hide[] = {};
+			verticalOffset = 0;
+			verticalOffsetWorld = 0;
+			init = "''";
+		};
+		editorPreview = "";
+		scope = 2;
+		displayName = "Diesel Locomotive 01";
+		model = "rnc_misc\railways\rnc_diesel_locomotive_01.p3d";
+		icon = "iconObject_5x2";
+		editorCategory = "rnc_miscObjects";
+		editorSubcategory = "rnc_mapmaking";
+		vehicleClass = "Structures_Industrial";
+		cost = 2000;
+		keepHorizontalPlacement = 0;
+	};
+	
+	class Land_rnc_diesel_locomotive_02: ThingX {
+		author = "RUMM & COKE Team / Quaternius";
+		mapSize = 6.1;
+		class SimpleObject {
+			eden = 0;
+			animate[] = {};
+			hide[] = {};
+			verticalOffset = 0;
+			verticalOffsetWorld = 0;
+			init = "''";
+		};
+		editorPreview = "";
+		scope = 2;
+		displayName = "Diesel Locomotive 02";
+		model = "rnc_misc\railways\rnc_diesel_locomotive_02.p3d";
+		icon = "iconObject_5x2";
+		editorCategory = "rnc_miscObjects";
+		editorSubcategory = "rnc_mapmaking";
+		vehicleClass = "Structures_Industrial";
+		cost = 2000;
+		keepHorizontalPlacement = 0;
+	};
+};
