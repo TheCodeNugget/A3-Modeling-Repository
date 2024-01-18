7e2d9d5caac216859656125d14f0fd1c88295c6e
TheCodeNugget <kyilmaz190300@gmail.com>
2024-01-18 21:50:55 +0300

    feat: HG billboard added

--- rnc_misc/Billboards/config.cpp
+++ rnc_misc/Billboards/config.cpp
@@ -72,4 +72,25 @@ class CfgVehicles {
 		editorCategory = "rnc_miscObjects";
 		editorSubcategory = "EdSubcat_Services";
 	};
+	
+	class Land_rnc_bb01_hgconsult: House_Small_F {
+		scope = 2;
+		mapSize = 34.4;
+		displayName = "Horrible Consult BB01";
+		icon = "iconObject_1x2";
+		model = "rnc_misc\Billboards\rnc_bb01_hgconsult.p3d";
+
+		class SimpleObject
+		{
+			eden = 0;
+			animate[] = {};
+			hide[] = {};
+			verticalOffset = 25.907;
+			verticalOffsetWorld = 0;
+			init = "''";
+		};
+	
+		editorCategory = "rnc_miscObjects";
+		editorSubcategory = "EdSubcat_Services";
+	};
 };
