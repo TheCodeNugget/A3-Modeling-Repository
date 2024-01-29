8d8347f77beebd6944bf211e1325b29a149c00fc
TheCodeNugget <kyilmaz190300@gmail.com>
2024-01-29 19:44:43 +0300

    feat: biplane added

--- rnc_misc/aircraft/config.cpp
+++ rnc_misc/aircraft/config.cpp
@@ -145,4 +145,27 @@ class CfgVehicles {
 		cost = 2000;
 		keepHorizontalPlacement = 0;
 	};
+	
+	class Land_passjet_biplane: House_F {
+		author = "RUMM & COKE Team / Quaternius";
+		mapSize = 6.1;
+		class SimpleObject {
+			eden = 0;
+			animate[] = {};
+			hide[] = {};
+			verticalOffset = 0;
+			verticalOffsetWorld = 0;
+			init = "''";
+		};
+		editorPreview = "";
+		scope = 2;
+		displayName = "Passenger Plane Biplane";
+		model = "rnc_misc\aircraft\rnc_pass_jet\rnc_passjet_biplane.p3d";
+		icon = "iconObject_5x2";
+		editorCategory = "rnc_miscObjects";
+		editorSubcategory = "EdSubcat_Planes";
+		vehicleClass = "Structures_Industrial";
+		cost = 2000;
+		keepHorizontalPlacement = 0;
+	};
 };
