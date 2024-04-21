6d8b6d8a8365accaa40fa641d806a2002ad5eaf2
KenTheNugget <95768094+TheCodeNugget@users.noreply.github.com>
2024-04-21 23:13:01 +0300

    feat: cerveza grenade added

--- nug_cerveza_cristal/config.cpp
+++ nug_cerveza_cristal/config.cpp
@@ -28,13 +28,18 @@ class CfgPatches
 {
 	class nug_cerveza_cristal
 	{
-		addonRootClass = "A3_Structures_F";
-		requiredAddons[] = {"A3_Structures_F_Items"};
+		requiredaddons[] = {"A3_Weapons_F"};
 		requiredVersion = 0.1;
-		units[] = {"Land_BottlePlastic_V1_F","Land_Can_Dented_F","Land_Can_Rusty_F","Land_Can_V1_F","Land_Can_V2_F","Land_Can_V3_F","Land_TacticalBacon_F"};
+		units[] = {};
 		weapons[] = {};
 	};
 };
+class Mode_SemiAuto;
+class Mode_Burst;
+class Mode_FullAuto;
+class SlotInfo;
+class CowsSlot;
+class ItemCore;
 class CfgVehicles
 {
 	class Items_base_F;
@@ -60,6 +65,40 @@ class CfgVehicles
 		icon = "iconObject_circle";
 		editorSubcategory = "EdSubcat_Food";
 	};
+
+	class All;
+	class Sound: All
+	{
+		author = "Ken The Nugget";
+		mapSize = 10;
+		_generalMacro = "Sound";
+		scope = 0;
+		side = -1;
+		animated = 0;
+		vehicleClass = "Sounds";
+		icon = "iconSound";
+		faction = "none";
+		editorCategory = "EdCat_Sounds";
+		editorSubcategory = "EdSubcat_Sounds";
+	};
+	class Nug_Sound_Cerveza_Cristal_Chorus: Sound
+	{
+		scope = 2;
+		sound = "Nug_Sound_Cerveza_Cristal_Chorus";
+		displayName = "Cerveza Cristal Chorus";
+	};
+	class Nug_Sound_Cerveza_Cristal: Sound
+	{
+		scope = 2;
+		sound = "Nug_Sound_Cerveza_Cristal";
+		displayName = "Cerveza Cristal";
+	};
+	class Nug_Sound_Cerveza_Cristal_Full: Sound
+	{
+		scope = 2;
+		sound = "Nug_Sound_Cerveza_Cristal_Full";
+		displayName = "Cerveza Cristal Full";
+	};
 };
 
 class CfgAmmo
@@ -75,6 +114,29 @@ class CfgAmmo
 		soundHit3[] = {"\nug_cerveza_cristal\data\impact.wss",2,1,2400};
 		soundHit4[] = {"\nug_cerveza_cristal\data\impact.wss",2,1,2400};
 		soundHit5[] = {"\nug_cerveza_cristal\data\impact.wss",2,1,2400};
+	};
+
+	class Grenade;
+	class CervezaAmmo: Grenade
+	{
+		hit = 10;
+		indirectHit = 10;
+		indirectHitRange = 7;
+		dangerRadiusHit = 36;
+		suppressionRadiusHit = 24;
+		typicalspeed = 18;
+		model = "\nug_cerveza_cristal\nug_cerveza_grenade.p3d";
+		//model = "\nug_cerveza_cristal\nug_cerveza_cristal.p3d";
+		visibleFire = 0.5;
+		audibleFire = 0.05;
+		visibleFireTime = 1;
+		fuseDistance = 0;
+		deflecting = 3;
+		//soundFly[] = {"\nug_cerveza_cristal\data\fly.wss",2,1,700};
+		//soundHit1[] = {"\nug_cerveza_cristal\data\impact.wss",2.5118864,1,1500};
+		//soundHit2[] = {"\nug_cerveza_cristal\data\impact.wss",2.5118864,1,1500};
+		//soundHit3[] = {"\nug_cerveza_cristal\data\impact.wss",2.5118864,1,1500};
+		//soundHit4[] = {"\nug_cerveza_cristal\data\impact.wss",2.5118864,1,1500};
 	};
 };
 
@@ -103,6 +165,27 @@ class CfgMagazines
 		pylonWeapon = "Cerveza_LGB_Launcher";
 		mass = 230;
 	};
+
+	class CA_Magazine;
+	class CervezaGrenade: CA_Magazine
+	{
+		author = "Ken The Nugget";
+		mass = 5;
+		scope = 2;
+		value = 1;
+		displayName = "Cerveza Cristal";
+		//picture = "";
+		model = "\nug_cerveza_cristal\nug_cerveza_grenade.p3d";
+		//model = "\nug_cerveza_cristal\nug_cerveza_cristal.p3d";
+		type = 256;
+		ammo = "CervezaAmmo";
+		count = 1;
+		initSpeed = 21;
+		nameSound = "handgrenade";
+		maxLeadSpeed = 7;
+		descriptionShort = "Cerveza Cristal";
+		displayNameShort = "Cerveza Cristal";
+	};
 };
 
 class CfgWeapons
@@ -119,5 +202,73 @@ class CfgWeapons
 		{
 			displayName = "Cerveza3";
 		};
+	};
+
+	class GrenadeLauncher;
+	class Throw: GrenadeLauncher
+	{
+		scope = 1;
+		autoAimEnabled = 0;
+		cursor = "EmptyCursor";
+		cursorAim = "throw";
+		value = 0;
+		type = 0;
+		displayName = "$STR_DN_THROW";
+		canDrop = 0;
+		textureType = "semi";
+		magazineReloadTime = 0;
+		reloadTime = 0;
+		muzzles[] += {"CervezaGrenadeMuzzle"};
+		class ThrowMuzzle: GrenadeLauncher
+		{
+			cursor = "EmptyCursor";
+			cursorAim = "throw";
+			sound[] = {"",0.00031622776,1};
+			reloadSound[] = {"",0.00031622776,1};
+			aiDispersionCoefX = 6;
+			aiDispersionCoefY = 6;
+			reloadTime = 0;
+			magazineReloadTime = 2.5;
+			enableAttack = 0;
+			showEmpty = 0;
+			autoReload = 1;
+			modelOptics = "";
+			minRange = 10;
+			minRangeProbab = 0.2;
+			midRange = 45;
+			midRangeProbab = 0.9;
+			maxRange = 60;
+			maxRangeProbab = 0.03;
+			keepInInventory = 1;
+		};
+		class CervezaGrenadeMuzzle: ThrowMuzzle
+		{
+			magazines[] = {"CervezaGrenade"};
+		};
+	};
+};
+
+class CfgSFX
+{
+	class Nug_Sound_Cerveza_Cristal_Chorus
+	{
+		name = "Cerveza Cristal Chorus";
+		sounds[] = {"sfxsound"};
+		sfxsound[] = {"\nug_cerveza_cristal\data\fly.wss",10,1,100,1,1,1,0};
+		empty[] = {"",0,0,0,0,0,0,0};
+	};
+	class Nug_Sound_Cerveza_Cristal
+	{
+		name = "Cerveza Cristal";
+		sounds[] = {"sfxsound"};
+		sfxsound[] = {"\nug_cerveza_cristal\data\impact.wss",10,1,100,1,3,3,3};
+		empty[] = {"",0,0,0,0,0,0,0};
+	};
+	class Nug_Sound_Cerveza_Cristal_Full
+	{
+		name = "Cerveza Cristal Full";
+		sounds[] = {"sfxsound"};
+		sfxsound[] = {"\nug_cerveza_cristal\data\cerveza_cristal.wss",12,1,100,1,3,3,3};
+		empty[] = {"",0,0,0,0,0,0,0};
 	};
 };
