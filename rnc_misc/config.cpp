efeac992862ed9cfcb41a8fd833c73df39211dc5
KenTheNugget <95768094+TheCodeNugget@users.noreply.github.com>
2024-08-25 22:14:43 +0300

    :rocket: kerfus packed

--- rnc_misc/config.cpp
+++ rnc_misc/config.cpp
@@ -189,6 +189,29 @@ class CfgVehicles {
 		model = "rnc_misc\rnc_rubber_duck_smol.p3d";
 	};
 
+	class Land_rnc_kerfus: House_Small_F {
+		author = "RUMM & COKE Team";
+		mapSize = 6.1;
+		class SimpleObject {
+			eden = 0;
+			animate[] = {};
+			hide[] = {};
+			verticalOffset = 1.358;
+			verticalOffsetWorld = 0;
+			init = "''";
+		};
+		editorPreview = "";
+		scope = 2;
+		displayName = "Kerfus";
+		model = "rnc_misc\kerfus\rnc_kerfus.p3d";
+		icon = "iconObject_5x2";
+		editorCategory = "rnc_structures";
+		editorSubcategory = "rnc_waterObj";
+		vehicleClass = "Structures_Industrial";
+		cost = 2000;
+		keepHorizontalPlacement = 0;
+	};
+
 	class Land_rnc_alwarren: House_Small_F {
 		author = "RUMM & COKE Team";
 		mapSize = 6.1;
